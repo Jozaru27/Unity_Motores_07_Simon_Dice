bedbe51891aa9a4e495821c57c8370d97551b746
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2025-02-12 09:23:39 +0100

    Extra 1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,8 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private string[] botonesPulsados = new string[20];
-    private int contadorBotonesPulsados = 0;
+    private string[] botonesPulsados = new string[20]; // Almacena la secuencia de botones pulsados
+    private int contadorBotonesPulsados = 0; // Lleva la cuenta de botones pulsados
     public TextMeshProUGUI textoColores;
 
     // Start is called before the first frame update
@@ -22,6 +22,7 @@ public class GameManager : MonoBehaviour
     }
 
 
+    // Almacena pulsaciones hasta 20, mostrando la elección al instante mediante el debug log
     public void RegistrarPulsacion(string color)
     {
         if (contadorBotonesPulsados < 20)
@@ -37,6 +38,7 @@ public class GameManager : MonoBehaviour
     }
 
 
+    // Muestra la secuencia de colores pulsados en el debug log
     public void MostrarSecuencia()
     {
         Debug.Log("Secuencia de colores pulsados:");
@@ -49,13 +51,14 @@ public class GameManager : MonoBehaviour
 
     public void IniciarMostrarSecuencia()
     {
-        StartCoroutine(MostrarSecuenciaTexto()); 
+        StartCoroutine(MostrarSecuenciaTexto()); // Llama a la corrutina para mostrar texto 
     }
 
 
+    // Muestra la secuencia de colores pulsados mediante texto en pantalla
     public IEnumerator MostrarSecuenciaTexto()
     {
-        textoColores.text = ""; 
+        textoColores.text = "";
         for (int i = 0; i < contadorBotonesPulsados; i++)
         {
             textoColores.text = botonesPulsados[i];
@@ -63,5 +66,16 @@ public class GameManager : MonoBehaviour
         }
         yield return new WaitForSeconds(1f);
         textoColores.text = "";
+
+        ReiniciarJuego(); // Una vez ha mostrado los colores, reinicia el juego.
+    }
+
+
+    // Reinicia el array y el contador para que se puedan almacenar nuevos colores
+    private void ReiniciarJuego()
+    {
+        botonesPulsados = new string[20];
+        contadorBotonesPulsados = 0;
+        Debug.Log("El juego se ha reiniciado. Puedes empezar a pulsar colores de nuevo.");
     }
 }
